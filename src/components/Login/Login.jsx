1becdef8c9d7f05a49bfda8db4f34044e4073bab
ShakirMahmud <shakirmahmud50@gmail.com>
2024-11-09 00:13:54 +0600

    Display user info and sign out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,52 @@
-import { GoogleAuthProvider } from 'firebase/auth/web-extension';
-import React from 'react';
+import { GoogleAuthProvider, signOut } from 'firebase/auth';
+import auth from '../../firebase/firebase.init';
+import { signInWithPopup } from 'firebase/auth';
+import { useState } from 'react';
 
 const Login = () => {
+    const [user, setUser] = useState(null)
 
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
-        
-        console.log('Google Sign-In successful');
+        console.log(auth, provider)
+        signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result);
+                setUser(result.user);
+            }).catch(err => {
+                console.log('ERROR', err);
+                setUser(null);
+            })
     };
+    const handleSignOut= ()=>{
+        signOut(auth)
+        .then(()=>{
+            console.log('Signing out');
+            setUser(null);
+        }).catch(err => console.log('ERROR', err))
+    }
 
     return (
         <div>
             <h2>Login</h2>
-            <button onClick={handleGoogleSignIn} className='p-4 mt-4 border-2 rounded-2xl'>Sign in with Google</button>
+            {/* <button onClick={handleGoogleSignIn} className='p-4 mt-4 border-2 rounded-2xl'>Sign in with Google</button>
+            <button onClick={handleSignOut} className='p-4 mt-4 border-2 rounded-2xl'>Sign Out</button> */}
+
+            {
+                user ? 
+                <button onClick={handleSignOut} className='p-4 mt-4 border-2 rounded-2xl'>Sign Out</button>
+                :
+                <button onClick={handleGoogleSignIn} className='p-4 mt-4 border-2 rounded-2xl'>Sign in with Google</button>
+            }
+
+            {
+                user && <div className='flex flex-col items-center space-y-4 mt-6'>
+                    <h4>Logged in as {user.displayName}</h4>
+                    <p>Email: {user.email}</p>
+                    <img className='rounded-full' src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
